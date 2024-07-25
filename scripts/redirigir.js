c832a17ae5d14a64474a9362c0d9867d4425d532
Luciano Seguel <96950349+lucianoseguel@users.noreply.github.com>
2024-07-24 23:04:51 -0300

    4.3 form updated

--- scripts/redirigir.js
+++ scripts/redirigir.js
@@ -2,3 +2,12 @@ function contactform() {
     window.location.href = 'form-contact.html'
     
 }
+
+
+const masinfo = document.querySelector('#btn-paq')
+
+const modal = document.querySelector('#modal')
+
+const enviar = document.querySelector('#submit')
+
+
